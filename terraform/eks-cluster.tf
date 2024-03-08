0aa608424cd4f1a02e9aa7c58461b85fcfabe92e
katarzyna-da-silva <conic916@gmail.com>
2024-03-08 11:08:55 +0100

    modif code for cluster terraform

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,5 +1,6 @@
 resource "aws_eks_access_entry" "eks-iam-role" {     //ressource for aws- eks 
-  cluster_name = cluster_eks.name    // name cluster 
-  principal_arn = aws_iam_role.eks-iam-role.arn    // eks-iam-role
-  type = "STANDARD"
+  cluster_name = cluster_eks.eks-iam-role.name    // name cluster 
+  principal_arn = aws_iam_role.eks-iam-role.arn    // eks-iam-role, role arn for user = access for cluster 
+  type = "STANDARD"  // 
 }
+
