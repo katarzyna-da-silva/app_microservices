b60198508fc5391b76e8f4b0be675b1fae9e43e4
katarzyna-da-silva <conic916@gmail.com>
2024-03-08 10:56:15 +0100

    add file for ressource cluster

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,24 +1,5 @@
-resource "aws_eks_cluster" "cluster" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.cluster.arn
-  version  = var.cluster_version
-
-  vpc_config {
-    subnet_ids = [aws_subnet.cluster.id, aws_subnet.cluster2.id]
-  }
-
-# cluster EKS dépend de l'inclusion de stratégies IAM. 
-# Terraform attendra le chargement de ces pièces jointes avant de créer le cluster EKS.
-depends_on = [
-    aws_iam_role_policy_attachment.cluster-AmazonEKSClusterPolicy,                       // gestion cluster eks
-    aws_iam_role_policy_attachment.cluster-AmazonEKSVPCResourceController,              //Ressource vpc 
-    ]
+resource "aws_eks_access_entry" "eks-iam-role" {     //ressource for aws- eks 
+  cluster_name = cluster_eks.name    // name cluster 
+  principal_arn = aws_iam_role.eks-iam-role.arn    // eks-iam-role
+  type = "STANDARD"
 }
-
-## Les sous-réseaux d'Amazon EKS sont utilisés pour organiser les ressources du cluster 
-## Kubernetes dans le cloud AWS, assurant l'isolation et la gestion de l'accès aux ressources 
-## en réseau telles que les instances EC2 et les ressources de conteneur. Ils permettent également 
-## la séparation et la gestion du trafic réseau, permettant le fonctionnement efficace du cluster EKS dans 
-## une zone réseau sélectionnée dans le VPC.
-
-
