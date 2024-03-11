03fda29cc2202302064b5e9a8554a525af0aa7ff
katarzyna-da-silva <conic916@gmail.com>
2024-03-11 13:25:20 +0100

    modif readme

--- terraform/eks-node-group-public.tf
+++ terraform/eks-node-group-public.tf
@@ -3,10 +3,11 @@
 
 resource "aws_eks_node_group" "eks_ng_public" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
+
   node_group_name = "${local.name}-eks-ng-public"
   node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
   subnet_ids      = module.vpc.public_subnets
-  version         = var.cluster_version
+  #version         = var.cluster_version
 
   scaling_config {
     desired_size = 1
